00eb932e120337ef39a41e6fc4a10a30b65ceb69
Jatin Lahari <laharijateenSAM@gmail.com>
2026-03-11 14:33:27 +0530

    production url added

--- main.py
+++ main.py
@@ -41,6 +41,8 @@ ALLOWED_ORIGINS = [
     "http://localhost:8080",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:8080",
+    "https://mygangour.samyotech.in",
+    "https://mygangour.com"
 ]
 
 app.add_middleware(
