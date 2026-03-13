8d2c0dd01dc9f6e2c768be867665f24f2e031859
Jatin Lahari <laharijateenSAM@gmail.com>
2026-03-13 16:43:42 +0530

    token minute update

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_MINUTES: int = 30
+    ACCESS_TOKEN_MINUTES: int = 600
     REFRESH_TOKEN_DAYS: int = 7
     MONGODB_URL: str
     REDIS_HOST: str = "localhost"
